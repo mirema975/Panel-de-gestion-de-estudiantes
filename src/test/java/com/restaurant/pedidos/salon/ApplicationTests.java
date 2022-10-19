c52f4af91635b6d3db0ecfcd70cb655afc0bf088
mirema975 <emanuelmiranda975@gmail.com>
2022-10-19 14:26:34 -0300

    Inicializacion del proyecto

---
+++ src/test/java/com/restaurant/pedidos/salon/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.pedidos.salon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
