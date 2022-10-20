05476a4e5a64123d2bc0e8a52acfafc0805d7942
mirema975 <emanuelmiranda975@gmail.com>
2022-10-19 21:33:44 -0300

    se agregaron las funciones crud al servicio de estudiante

--- src/main/java/com/restaurant/pedidos/salon/service/EstudianteService.java
+++ src/main/java/com/restaurant/pedidos/salon/service/EstudianteService.java
@@ -7,4 +7,12 @@ import com.restaurant.pedidos.salon.entity.Estudiante;
 public interface EstudianteService {
     
     public List<Estudiante> listarTodosLosEstudiantes();
+
+    public Estudiante guardarEstudiante(Estudiante estudiante);
+
+    public Estudiante obtenerEstudiantePorId(Long id);
+
+    public Estudiante actualizarEstudiante(Estudiante estudiante);
+
+    public void eliminarEstudiante(Long id);
 }
--- src/main/java/com/restaurant/pedidos/salon/service/EstudianteServiceImpl.java
+++ src/main/java/com/restaurant/pedidos/salon/service/EstudianteServiceImpl.java
@@ -1,6 +1,7 @@
 package com.restaurant.pedidos.salon.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,31 @@ public class EstudianteServiceImpl implements EstudianteService {
     @Override
     public List<Estudiante> listarTodosLosEstudiantes() {
         return repository.findAll();
+    }
+
+    @Override
+    public Estudiante guardarEstudiante(Estudiante estudiante) {
+        return repository.save(estudiante);
+    }
+
+    @Override
+    public Estudiante obtenerEstudiantePorId(Long id) {
+        Optional<Estudiante> estudiante = repository.findById(id);
+        if(estudiante.isPresent()) 
+            return estudiante.get();
+        return null;
+
+    }
+
+    @Override
+    public Estudiante actualizarEstudiante(Estudiante estudiante) {
+        return repository.save(estudiante);
+    }
+
+    @Override
+    public void eliminarEstudiante(Long id) {
+        repository.deleteById(id);
+        
     }
 
     
