730f1c6b2b94452e7d118fde03c713ba5e245ca5
mirema975 <emanuelmiranda975@gmail.com>
2022-10-19 21:32:44 -0300

    creacion del controlador de estudiante

--- src/main/java/com/restaurant/pedidos/salon/controller/EstudianteController.java
+++ src/main/java/com/restaurant/pedidos/salon/controller/EstudianteController.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.restaurant.pedidos.salon.entity.Estudiante;
 import com.restaurant.pedidos.salon.service.EstudianteService;
 
 @Controller
@@ -19,4 +23,41 @@ public class EstudianteController {
         return "estudiantes";
     }
 
+    @GetMapping("/estudiantes/nuevo")
+    public String formularioRegistroEstudiante(Model modelo) {
+        Estudiante estudiante = new Estudiante();
+        modelo.addAttribute("estudiante", estudiante);
+        return "crear_estudiante";
+    }
+
+    @PostMapping("/estudiantes")
+    public String guardarEstudiante(@ModelAttribute("estudiante") Estudiante estudiante){
+        estudianteService.guardarEstudiante(estudiante);
+        return "redirect:/estudiantes";
+    }
+
+    @GetMapping("/estudiantes/editar/{id}")
+    public String mostrarFomularioEditar(@PathVariable Long id, Model modelo){
+        modelo.addAttribute("estudiante", estudianteService.obtenerEstudiantePorId(id));
+        return "editar_estudiante";
+    }
+
+    @PostMapping("estudiantes/{id}")
+    public String actualizarEstudiante(@PathVariable Long id, @ModelAttribute("estudiante") Estudiante estudiante, Model modelo){
+        Estudiante estudianteExistente = estudianteService.obtenerEstudiantePorId(id);
+        estudianteExistente.setId(id);
+        estudianteExistente.setNombre(estudiante.getNombre());
+        estudianteExistente.setApellido(estudiante.getApellido());
+        estudianteExistente.setEmail(estudiante.getEmail());
+
+        estudianteService.actualizarEstudiante(estudianteExistente);
+        return "redirect:/estudiantes";
+    }
+
+    @GetMapping("/estudiantes/{id}")
+    public String eliminarEstudiante(@PathVariable Long id){
+        estudianteService.eliminarEstudiante(id);
+        return "redirect:/estudiantes";
+    }
 }
+
